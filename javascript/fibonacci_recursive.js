7f2c0569b72322f406db2d0919926aed17dd0917
Walker Le <walker.le88@gmail.com>
2023-06-20 23:13:21 -0600

    javascript and ruby complete

--- javascript/fibonacci_recursive.js
+++ javascript/fibonacci_recursive.js
@@ -1,11 +1,23 @@
 function fibonacci(n) {
   // type your code here
+  if(n < 2) {
+    // console.log('n =', n);
+    return n;
+  }
+  
+  // console.log('n =', n);
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 0");
   console.log("=>", fibonacci(0));
+
+  console.log("");
+
+  console.log("Expecting: 1");
+  console.log("=>", fibonacci(1));
 
   console.log("");
 
